1bf2acd04831ab82a67f2f7c99b615ac8f8d772e
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2026-01-11 13:25:33 +0530

    add day03

--- lessons/day03/main.tf
+++ lessons/day03/main.tf
@@ -1,24 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  # Configuration options
-    region = "us-east-1"
+  region = "ap-south-1"
 }
 
-# Create a S3 bucket
-resource "aws_s3_bucket" "tf_test_baivab_bucket" {
-  bucket = "my-tf-test-baiv-bucket-101"
+# Create a S3 Bucket
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "terraform-demo-bucket-sk-12345" # Change to a unique name
 
   tags = {
-    Name        = "My bucket"
+    # Name        = "My bucket" # Change and tf apply again to see update in tags
+    Name        = "My bucket 2.0"
     Environment = "Dev"
   }
 }
-
