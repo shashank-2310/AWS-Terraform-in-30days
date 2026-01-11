a2cf640db55654bb32cfbe21760cef82782da53c
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2026-01-11 22:36:32 +0530

    day07

--- lessons/day07/output.tf
+++
@@ -1,8 +0,0 @@
-# Output variables for printing after apply and passing to other configurations
-# output "vpc_id" {
-#     value = aws_vpc.demo_vpc.id
-# }
-
-# output "ec2_id" {
-#     value = aws_instance.demo_ec2.id
-# }
