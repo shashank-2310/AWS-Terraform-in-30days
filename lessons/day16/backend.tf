c65037a2cf7e6c11d017ff35c5ee24ebcc8b3dfc
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2026-01-26 21:10:41 +0530

    Revert "Day16: AWS IAM User Management with Terraform"
    This reverts commit 4947d394a5e35988ccf262e4f68395f27239377d.

--- lessons/day16/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "skdevops-terraform-state" # Change to your unique bucket name
-    key    = "dev/terraform.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-    use_lockfile = true
-  }
-}
